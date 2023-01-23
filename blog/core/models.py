e7b874d94a883b05bbeed9a7584dfbe2e6bb3af8
João Bruno <jbrunomf@outlook.com>
2023-01-23 19:43:12 -0400

    atualizando

--- blog/core/models.py
+++ blog/core/models.py
@@ -1,4 +1,5 @@
 from datetime import timezone
+import datetime
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -13,13 +14,13 @@ class Post(models.Model):
     slug = models.SlugField(max_length=250, unique_for_date='publish')
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
     body = models.TextField()
-    publish = models.DateTimeField(default=timezone.now)
+    publish = models.DateTimeField(default=datetime.datetime.now)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
 
     class Meta:
-        ordering = ('-publish')
+        ordering = ('-publish',)
 
     def __str__(self) -> str:
         return self.title

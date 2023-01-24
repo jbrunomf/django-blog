4b8da5a31d2aac327e16ba938d0b78d2d0465b69
João Bruno <jbrunomf@outlook.com>
2023-01-23 20:17:49 -0400

    Atualizando Modelo

--- blog/core/models.py
+++ blog/core/models.py
@@ -5,6 +5,13 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager, self)\
+            .get_queryset()\
+                .filter(status='published')
+
+
 class Post(models.Model):
     STATUS_CHOICES = (
         ('draft', 'Draft'),
@@ -18,6 +25,8 @@ class Post(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
+    objects = models.Manager() # manager default
+    published = PublishedManager() # manager customizado
 
     class Meta:
         ordering = ('-publish',)

9baa4087e24113e37d197128637f51df6043c948
João Bruno <jbrunomf@outlook.com>
2023-01-24 20:27:44 -0400

    Refatorando para class based views

--- blog/core/views.py
+++ blog/core/views.py
@@ -1,31 +1,22 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+
 from .models import Post
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 # Create your views here.
-
-
-def post_list(request):
-    object_list = Post.published.all()
-    paginator = Paginator(object_list, 3) #três postagens por página
-    page = request.GET.get('page')
-    try:
-        posts = paginator.page(page)
-    except PageNotAnInteger:
-        # Se a página não for um inteiro, exibe a primeira pagina
-        posts = paginator.page(1)
-    except EmptyPage:
-        # Se a página estiver fora do intervalo
-        # Exibe a ultima pagina de resultados
-        posts = paginator.page(paginator.num_pages)
-    return render(request, 'core/post/list.html', {'page': page, 'posts': posts})
-
-
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, slug=post, status='published', 
     publish__year=year, publish__month=month, publish__day=day)
     
     return render(request, 'core/post/detail.html', {'post': post})
+
+
+class PostListView(ListView):
+    queryset = Post.published.all().filter(publish__year='2023')
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'core/post/list.html'
 

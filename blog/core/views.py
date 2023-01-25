6594f716c8fda7a96a4d145d7276b3ff387c6c7b
João Bruno <jbrunomf@outlook.com>
2023-01-24 20:16:20 -0400

    Adicionando paginação na lista de postagens do blog.

--- blog/core/views.py
+++ blog/core/views.py
@@ -1,11 +1,27 @@
 from django.shortcuts import render
 from .models import Post
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
+
 # Create your views here.
 
+
 def post_list(request):
-    posts = Post.published.all()
-    return render(request, 'core/post/list.html', {'posts': posts})
+    object_list = Post.published.all()
+    paginator = Paginator(object_list, 3) #três postagens por página
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        # Se a página não for um inteiro, exibe a primeira pagina
+        posts = paginator.page(1)
+    except EmptyPage:
+        # Se a página estiver fora do intervalo
+        # Exibe a ultima pagina de resultados
+        posts = paginator.page(paginator.num_pages)
+    return render(request, 'core/post/list.html', {'page': page, 'posts': posts})
+
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, slug=post, status='published', 
